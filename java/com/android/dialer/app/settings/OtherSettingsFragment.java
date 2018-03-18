3bd334c6e45a08e14bfc7af7a9f52730e4c949c1
blunden <blunden2@gmail.com>
2018-03-18 14:02:30 +0100

    Add setting to enable Do Not Disturb during calls
    Add DND Permission by default. Those who don't want can disable dnd switch
    
    Android N and earlier enabled Do Not Disturb mode while in a call.
    Reimplement this behavior to prevent incoming notifications from
    vibrating or playing sounds.
    
    Change-Id: Ic38ac775c6e353898190e1571b6d6e521c0e5aa1
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>
    Signed-off-by: phenomenall <venkysaihot@gmail.com>

--- java/com/android/dialer/app/settings/OtherSettingsFragment.java
+++ java/com/android/dialer/app/settings/OtherSettingsFragment.java
@@ -16,7 +16,11 @@
 
 package com.android.dialer.app.settings;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -38,6 +42,8 @@ public class OtherSettingsFragment extends PreferenceFragment
 
   private SwitchPreference mEnablePostcall;
   private SwitchPreference mFullscreenCallerPhoto;
+  private SwitchPreference enableDndInCall;
+  private NotificationManager notificationManager;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -55,6 +61,9 @@ public class OtherSettingsFragment extends PreferenceFragment
     mEnablePostcall.setChecked(mEnabled);
     mEnablePostcall.setOnPreferenceChangeListener(this);
 
+    enableDndInCall = (SwitchPreference) findPreference("incall_enable_dnd");
+    enableDndInCall.setOnPreferenceChangeListener(this);
+
     if (!CallRecorderService.isEnabled(getActivity())) {
       getPreferenceScreen().removePreference(
           findPreference(context.getString(R.string.call_recording_category_key)));
@@ -63,6 +72,8 @@ public class OtherSettingsFragment extends PreferenceFragment
     mFullscreenCallerPhoto = (SwitchPreference) findPreference(FULLSCREEN_CALLER_PHOTO);
     mFullscreenCallerPhoto.setChecked(mPrefs.getBoolean(FULLSCREEN_CALLER_PHOTO, false));
     mFullscreenCallerPhoto.setOnPreferenceChangeListener(this);
+
+    notificationManager = context.getSystemService(NotificationManager.class);
   }
 
   @Override
@@ -81,6 +92,29 @@ public class OtherSettingsFragment extends PreferenceFragment
           .putBoolean(FULLSCREEN_CALLER_PHOTO, value)
           .apply();
         return true;
+    } else if (preference == enableDndInCall) {
+      boolean newValue = (Boolean) objValue;
+      if (newValue && !notificationManager.isNotificationPolicyAccessGranted()) {
+        new AlertDialog.Builder(getContext())
+            .setMessage(R.string.incall_dnd_dialog_message)
+            .setPositiveButton(R.string.allow, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                startActivity(intent);
+              }
+            })
+            .setNegativeButton(R.string.deny, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+              }
+            })
+            .show();
+        // At this time, it is unknown whether the user granted the permission
+        return true;
+      }
     }
     return false;
   }
